ae003157eb3ff2bf1f43a73aabd57db6b386c126
Aldref27 <mariodangelo1996@gmail.com>
2025-05-29 19:18:20 +0200

    feat: javafx start login page

---
+++ poo/javafx-demo/src/main/java/com/progetto/controller/App.java
@@ -0,0 +1,22 @@
+package com.progetto.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/loginpage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.show();
+    }
+}
---
+++ poo/javafx-demo/src/main/java/com/progetto/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.progetto.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class LoginController {
+    @FXML
+        private void Loginclick(ActionEvent event) {
+            System.out.println("Login button clicked!");
+            
+        }
+}
